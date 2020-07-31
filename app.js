4ee698ddcbd83dec04d80586c862638b6cd2468f
Leo Ayala <leo_a1016@yahoo.com>
2020-07-31 18:34:45 -0400

    added ability to create HTML file

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    
-      console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-      profileDataArr.forEach(profileItem => console.log(profileItem));
-    }
+const [name, github] = profileDataArgs;
+
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
   };
-  printProfileData(profileDataArgs);
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       profileDataArr.forEach(profileItem => console.log(profileItem));
+      
+//       console.log('================');
+      
+//     }
+//   };
+//   printProfileData(profileDataArgs);
