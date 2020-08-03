1142025e1da5d934dcb80d3283be3f7a8f1bda17
Leo Ayala <leo_a1016@yahoo.com>
2020-08-02 20:27:56 -0400

    finished coding for HTML layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-
+const generatePage = require('./src/page-template');
+const fs = require('fs');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -121,15 +122,20 @@ const promptUser = () => {
         }
       })
     };
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+      const pageHTML = generatePage(portfolioData);
+  
+      fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+  
+        console.log('Page created! Check out index.html in this directory to see it!');
+      });
+    });
 
 
     // const fs = require('fs');
-// const generatePage = require('./src/page-template');
 
 // const profileDataArgs = process.argv.slice(2);
 
@@ -154,4 +160,4 @@ const promptUser = () => {
       
 //     }
 //   };
-//   printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs)
